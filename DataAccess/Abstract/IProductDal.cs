3c8f6d0e0856f0087e77eaaa4462a39f61ea7173
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-10-10 17:44:18 +0300

    .

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // product ile ilgili DB CRUD işlemlerimiz
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+    }
+}
