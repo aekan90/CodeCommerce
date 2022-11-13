2774c24dcda79bb99ec5536dd231dfae9ca5eff2
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-11-13 16:49:50 +0300

    9. Gün | 1.30.33

---
+++ Core/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,71 @@
+﻿using Core.DataAccess;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+         where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity); // referansı yakala
+                addedEntity.State = EntityState.Added; // bu referans eklenecek bir nesnedir
+                context.SaveChanges(); //  değişikliği uygula
+            }
+        }
+        public void DIPtest(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity); // referansı yakala
+                deletedEntity.State = EntityState.Deleted; // bu referans eklenecek bir nesnedir
+                context.SaveChanges(); //  değişikliği uygula
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity); // referansı yakala
+                updatedEntity.State = EntityState.Modified; // bu referans eklenecek bir nesnedir
+                context.SaveChanges(); //  değişikliği uygula
+            }
+        }
+    }
+
+}
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,62 +1,13 @@
-﻿using DataAccess.Abstract;
+﻿using Core.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEntity = context.Entry(entity); // referansı yakala
-                addedEntity.State = EntityState.Added; // bu referans eklenecek bir nesnedir
-                context.SaveChanges(); //  değişikliği uygula
-            }
-        }
-        public void DIPtest(string txt)
-        {
-            Console.WriteLine(txt);
-        }
 
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var deletedEntity = context.Entry(entity); // referansı yakala
-                deletedEntity.State = EntityState.Deleted; // bu referans eklenecek bir nesnedir
-                context.SaveChanges(); //  değişikliği uygula
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var updatedEntity = context.Entry(entity); // referansı yakala
-                updatedEntity.State = EntityState.Modified; // bu referans eklenecek bir nesnedir
-                context.SaveChanges(); //  değişikliği uygula
-            }
-        }
     }
 }
