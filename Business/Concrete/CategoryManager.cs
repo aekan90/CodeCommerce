b2a49aac27bac222b6ed5e45c1b82893e379c30e
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-11-14 17:52:02 +0300

    .

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal categoryDal;
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllByCustomerId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
