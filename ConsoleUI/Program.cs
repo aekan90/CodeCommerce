41f3c8b352f0997678f5d5dd4aa9817cc83da9f4
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-11-21 17:30:19 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,27 +5,41 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //ProductManager productManager = new ProductManager(new EfProductDal());
+        //_ProductManagerTest();
+        Console.WriteLine("-----------------------------------");
+        //_OrderManagerTest();
+        Console.WriteLine("-----------------------------------");
+        //_CategoryManagerTest();
+    }
 
-        //foreach (var item in productManager.GetByUnitPrice(10, 20)) // DIP : üstclass ref= new altclass(); ref.sadeceüstclassta ve alt classda olan metotlar gelir.(Bağımlılıkların tersine çevrilmesi)
-        //{
-        //    Console.WriteLine(item.ProductName);
-        //}
+    private static void _OrderManagerTest()
+    {
+        OrderManager orderManager = new OrderManager(new EfOrderDal());
 
-        //OrderManager orderManager = new OrderManager(new EfOrderDal());
+        foreach (var item in orderManager.GetAll())
+        {
+            Console.WriteLine(item.OrderID + " " + item.CustomerId);
+        }
 
-        //foreach (var item in orderManager.GetAll())
-        //{
-        //    Console.WriteLine(item.OrderID + " " + item.CustomerId);
-        //}
+    }
 
+    private static void _ProductManagerTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
 
+        foreach (var item in productManager.GetByUnitPrice(10, 20)) // DIP : üstclass ref= new altclass(); ref.sadeceüstclassta ve alt classda olan metotlar gelir.(Bağımlılıkların tersine çevrilmesi)
+        {
+            Console.WriteLine(item.ProductName);
+        }
+    }
+
+    private static void _CategoryManagerTest()
+    {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
-        foreach (var item in categoryManager.GetById(2))
+        foreach (var item in categoryManager.GetAll())
         {
             Console.WriteLine(item.CategoryName);
         }
-
     }
 }
