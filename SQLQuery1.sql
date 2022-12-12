9cc3306c546e8b90b03c27975345aade7b979005
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-12-12 12:54:23 +0300

    ..

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿SELECT Address as Adresi,City as Şehir FROM Customers
+
+SELECT City as Şehir,* FROM Customers WHERE City='London' or City='Berlin'
+
+SELECT * FROM PRODUCTS WHERE CATEGORYID=1 OR CATEGORYID=2
+
+
+SELECT * FROM PRODUCTS WHERE CATEGORYID=1 OR CATEGORYID=2 ORDER BY ProductName DESC
+
+
+SELECT COUNT(*) FROM PRODUCTS WHERE CategoryID=2
+
+-----------------------------------------------------------------------------
+
