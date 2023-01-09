1a863d881db327a49af7632f77c1d1629bb78b6e
Atıf Ertugrul Kan <aekan90@hotmail.com>
2023-01-09 11:31:13 +0300

    1

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -13,7 +13,7 @@ namespace Business.Abstract
         IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
         IResult Delete(Product product);
-    }
+    } 
 }
 
 
